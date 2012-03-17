1b2d7ca057833ee399efcac0cfdb31808f693f94
Oliver Brendel <admin@shinobilegends.com>
2012-03-17 15:50:47 +0000

    Version 1.2.3.+nb final

---
+++ mailinfo_server.php
@@ -0,0 +1,28 @@
+<?php
+
+define("OVERRIDE_FORCED_NAV",true);
+require("common.php");
+
+function mail_status($args) {
+	$new=maillink();
+	$objResponse = new xajaxResponse();
+	$objResponse->assign("maillink","innerHTML", $new);
+	global $session;
+	$warning='';
+	$timeout=strtotime($session['user']['laston'])-strtotime(date("Y-m-d H:i:s",strtotime("-".getsetting("LOGINTIMEOUT",900)." seconds")));
+	if ($timeout<200){
+		$warning="<br>".appoencode("`t").sprintf("TIMEOUT in %s seconds!",$timeout);
+	} elseif ($timeout<0) {
+		$warning="<br>".appoencode("`t")."Your session has timed out. Please log in again.";
+	} else $warning='';
+	$objResponse->assign("notify","innerHTML", $warning);
+	return $objResponse;
+}
+require("mailinfo_common.php");
+$xajax->processRequest();
+
+
+
+
+
+?>
