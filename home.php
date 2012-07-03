35b8c8f1744409e3533fe9df26062406aac2f9b2
Kneji <Kneji@f2a73b5d-fd4d-4f88-8b5b-290abe32d612>
2012-07-03 06:37:21 +0000

    text addon

--- home.php
+++ home.php
@@ -107,6 +107,7 @@ if ($onlinecount<getsetting("maxonline",0) || getsetting("maxonline",0)==0){
 	$pass = translate_inline("<u>P</u>assword");
 	$butt = translate_inline("Log in");
 	rawoutput("<form action='login.php' method='POST' onSubmit=\"md5pass();\">".templatereplace("login",array("username"=>$uname,"password"=>$pass,"button"=>$butt))."</form>");
+	output("Did you forget your password? Go <a href='create.php?op=forgot'>here</a> to retrieve a new one!`n",true);
 	output_notl("`c");
 	addnav("","login.php");
 } else {
