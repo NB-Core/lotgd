b6c8e9043c94e55075257a428720c2872c860e00
Oliver Brendel <admin@shinobilegends.com>
2013-05-10 11:26:34 +0000

    default add in

--- lib/commentary.php
+++ lib/commentary.php
@@ -341,11 +341,11 @@ function viewcommentary($section,$message="Interject your own commentary?",$limi
 				// disabled until I make the user prefs into their own good-fucking table because it is SO annoying to unserialize THEM!
 				// update: you can now set empty - if there should be an option for *specific accounts* to hide/show the tag, I will ignore it for now (workload)
 				if (($row['superuser']&SU_IS_GAMEMASTER)==SU_IS_GAMEMASTER) {
-					$chat_tag=getsetting('chat_tag_gm','');
+					$chat_tag=getsetting('chat_tag_gm','[GM]');
 					$row['name']="`\$".$chat_tag_gm."`0".$row['name'];
 				}
 				if (($row['superuser']&SU_EDIT_COMMENTS)==SU_EDIT_COMMENTS) {
-					$chat_tag_mod=getsetting('chat_tag_mod','');
+					$chat_tag_mod=getsetting('chat_tag_mod','[MOD]');
 					$row['name']="`\$".$chat_tag_mod."`0".$row['name'];
 				}
 			}
