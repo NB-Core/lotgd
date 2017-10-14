808b7b36ffded95c05755ed20c5ec046d7a38dc8
Oliver Brendel <admin@shinobilegends.com>
2017-10-14 09:31:33 +0000

    mailinfo functions should not produce a huge 302 error page when only need a short ajax message (bandwith usage)

--- mailinfo_server.php
+++ mailinfo_server.php
@@ -1,7 +1,14 @@
 <?php
 
 define("OVERRIDE_FORCED_NAV",true);
-require("common.php");
+/* you need to check if somebody timed out.
+if you call common.php and we have a timeout, he will the redirect to index.php?op=timeout, resulting in a full page
+which will (called in 1s intervals) download a lot of useless traffic to him and from your server
+
+therefore, a common.php is used that will not do a DO_FORCED_NAVIGATION.
+This will just make our mailinfo return a small string in case of a timeout, not an entire error page
+*/ 
+require("mailinfo_common.php");
 
 function mail_status($args=false) {
 	if ($args===false) return;
@@ -24,7 +31,9 @@ function mail_status($args=false) {
 	$timeout=strtotime($session['user']['laston'])-strtotime(date("Y-m-d H:i:s",strtotime("-".getsetting("LOGINTIMEOUT",900)." seconds")));
 	if ($timeout<=1) {
 		$warning="<br/>".appoencode("`\$`b")."Your session has timed out!".appoencode("`b");
-	} elseif ($timeout<12000){
+		session_unset();    
+		session_destroy(); // destroy if timeout
+	} elseif ($timeout<1800){
 		if ($timeout>60) {
 			$min = floor($timeout/60);
 			$timeout = $timeout-$min*60;
