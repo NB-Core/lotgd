98a0f478efe1b5a266a83cc9587cfa716c365a49
Oliver Brendel <admin@shinobilegends.com>
2022-10-21 23:31:54 +0200

    new composer stuff

--- ext/vendor/composer/installed.php
+++ ext/vendor/composer/installed.php
@@ -1,105 +1,104 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9d59102cfa25e55bbf37e1c55b7d75ca79b95ad2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
     ),
-    'reference' => '9ddc835d37f56b87a883f5fcf34dfd68c7a51efd',
-    'name' => '__root__',
-  ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '9ddc835d37f56b87a883f5fcf34dfd68c7a51efd',
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9d59102cfa25e55bbf37e1c55b7d75ca79b95ad2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'aura/session' => array(
+            'pretty_version' => '2.1.0',
+            'version' => '2.1.0.0',
+            'reference' => '7d2f7d41ad693970b5b6b83facca0961d3378883',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../aura/session',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jaxon-php/jaxon-armada' => array(
+            'pretty_version' => 'v2.0.1',
+            'version' => '2.0.1.0',
+            'reference' => '20b8557fe2703fa35acaea671cbf52ed36955ee8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jaxon-php/jaxon-armada',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jaxon-php/jaxon-core' => array(
+            'pretty_version' => 'v2.2.16',
+            'version' => '2.2.16.0',
+            'reference' => '386b24725ec28ca29280b15e0f0895a910aeed3a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jaxon-php/jaxon-core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jaxon-php/jaxon-sentry' => array(
+            'pretty_version' => 'v2.0.7',
+            'version' => '2.0.7.0',
+            'reference' => 'ab768cc2fe5c636c4e403f1288c83899ed1ffa1b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jaxon-php/jaxon-sentry',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'lemonphp/event' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '57d9ffcc551febca967068668dd9f005d989d94b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../lemonphp/event',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'matthiasmullie/minify' => array(
+            'pretty_version' => '1.3.69',
+            'version' => '1.3.69.0',
+            'reference' => 'a61c949cccd086808063611ef9698eabe42ef22f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../matthiasmullie/minify',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'matthiasmullie/path-converter' => array(
+            'pretty_version' => '1.1.3',
+            'version' => '1.1.3.0',
+            'reference' => 'e7d13b2c7e2f2268e1424aaed02085518afa02d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../matthiasmullie/path-converter',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.5',
+            'version' => '6.6.5.0',
+            'reference' => '8b6386d7417526d1ea4da9edb70b8352f7543627',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pimple/pimple' => array(
+            'pretty_version' => 'v3.0.2',
+            'version' => '3.0.2.0',
+            'reference' => 'a30f7d6e57565a2e1a316e1baf2a483f788b258a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pimple/pimple',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
     ),
-    'aura/session' => 
-    array (
-      'pretty_version' => '2.1.0',
-      'version' => '2.1.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '7d2f7d41ad693970b5b6b83facca0961d3378883',
-    ),
-    'jaxon-php/jaxon-armada' => 
-    array (
-      'pretty_version' => 'v2.0.1',
-      'version' => '2.0.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '20b8557fe2703fa35acaea671cbf52ed36955ee8',
-    ),
-    'jaxon-php/jaxon-core' => 
-    array (
-      'pretty_version' => 'v2.2.16',
-      'version' => '2.2.16.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '386b24725ec28ca29280b15e0f0895a910aeed3a',
-    ),
-    'jaxon-php/jaxon-sentry' => 
-    array (
-      'pretty_version' => 'v2.0.7',
-      'version' => '2.0.7.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'ab768cc2fe5c636c4e403f1288c83899ed1ffa1b',
-    ),
-    'lemonphp/event' => 
-    array (
-      'pretty_version' => 'v1.0.0',
-      'version' => '1.0.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '57d9ffcc551febca967068668dd9f005d989d94b',
-    ),
-    'matthiasmullie/minify' => 
-    array (
-      'pretty_version' => '1.3.66',
-      'version' => '1.3.66.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '45fd3b0f1dfa2c965857c6d4a470bea52adc31a6',
-    ),
-    'matthiasmullie/path-converter' => 
-    array (
-      'pretty_version' => '1.1.3',
-      'version' => '1.1.3.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'e7d13b2c7e2f2268e1424aaed02085518afa02d9',
-    ),
-    'phpmailer/phpmailer' => 
-    array (
-      'pretty_version' => 'v6.5.1',
-      'version' => '6.5.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'dd803df5ad7492e1b40637f7ebd258fee5ca7355',
-    ),
-    'pimple/pimple' => 
-    array (
-      'pretty_version' => 'v3.0.2',
-      'version' => '3.0.2.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'a30f7d6e57565a2e1a316e1baf2a483f788b258a',
-    ),
-  ),
 );
