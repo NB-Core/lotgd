0b8d729cff80c2df7816f828b9c1e2b13b5b248c
Oliver <admin@shinobilegends.com>
2024-11-10 10:41:10 +0100

    still fiddling with unserialization.... harder to catch than pokemon, some

--- lib/serialization.php
+++ lib/serialization.php
@@ -8,85 +8,64 @@ function safe_unserialize($data) {
         return $data;
     }
 
-    // Trim whitespace
     $data = trim($data);
 
-    // Check if the data is serialized
-    if (is_serialized($data)) {
+    // Check if the data is serialized and valid
+    if (is_valid_serialized($data)) {
+        // Set a custom error handler to suppress warnings/notices
+        set_error_handler(function() {
+            // Do nothing
+            return true;
+        });
+
         try {
-            // Suppress warnings with '@' and attempt to unserialize
-            $result = @unserialize($data);
+            $result = unserialize($data, ['allowed_classes' => false]);
+            restore_error_handler();
 
-            if ($result === false && $data !== 'b:0;') {
-                // Return original data if unserialization failed
-                return $data;
-            }
-
-            return $result;
-        } catch (\Exception $e) {
-            // Return original data on exception
-            return $data;
-        } catch (\Error $e) {
-            // Return original data on error
+            return $result !== false || $data === 'b:0;' ? $result : $data;
+        } catch (\Throwable $e) {
+            restore_error_handler();
+            // Return original data on exception/error
             return $data;
         }
     } else {
-        // Data is not serialized, return it as is
+        // Data is not serialized or is invalid, return it as is
         return $data;
     }
 }
 
+
 // Helper function to check if data is serialized
-function is_serialized($data) {
-    if (!is_string($data)) {
+function is_valid_serialized($data) {
+    // Basic checks
+    if (!is_string($data) || trim($data) === '') {
         return false;
     }
 
     $data = trim($data);
 
-    if ($data === 'N;') {
+    // Early return if data is 'N;' (serialized null) or 'b:0;' (serialized false)
+    if ($data === 'N;' || $data === 'b:0;') {
         return true;
     }
-    if ($data === 'b:0;') {
+
+    // Check if the data matches the serialized data pattern
+    $pattern = '/^([adObis]):/';
+    if (!preg_match($pattern, $data, $matches)) {
+        return false;
+    }
+
+    // Attempt to unserialize with error suppression
+    set_error_handler(function() {
+        // Suppress errors
         return true;
-    }
+    });
 
-    $length = strlen($data);
-    if ($length < 4) {
-        return false;
-    }
+    $result = @unserialize($data, ['allowed_classes' => false]);
+    restore_error_handler();
 
-    if ($data[1] !== ':') {
-        return false;
-    }
-
-    $lastChar = $data[$length - 1];
-    if ($lastChar !== ';' && $lastChar !== '}') {
-        return false;
-    }
-
-    $token = $data[0];
-    switch ($token) {
-        case 's':
-            if (preg_match('/^s:[0-9]+:.*(;|})$/s', $data)) {
-                return true;
-            }
-            break;
-        case 'a':
-        case 'O':
-            if (preg_match("/^{$token}:[0-9]+:.*[;}]\$/s", $data)) {
-                return true;
-            }
-            break;
-        case 'b':
-        case 'i':
-        case 'd':
-            if (preg_match("/^{$token}:[0-9.E-]+;\$/", $data)) {
-                return true;
-            }
-            break;
-    }
-
-    return false;
+    // If unserialize didn't return false or is 'b:0;', it's valid
+    return $result !== false || $data === 'b:0;';
 }
+
 
