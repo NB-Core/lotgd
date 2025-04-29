d1324777a3db3cff0b2fe1de19c4558660690ca7
Oliver <admin@shinobilegends.com>
2025-04-29 15:42:44 +0200

    fix and check in deathmessage ($extrarep[0] was empty sometimes)

--- lib/deathmessage.php
+++ lib/deathmessage.php
@@ -28,26 +28,32 @@ function select_deathmessage($forest=true,$extra=array(),$extrarep=array()) {
 	return array("deathmessage"=>$deathmessage,"taunt"=>$taunt);
 }
 
-function select_deathmessage_array($forest=true,$extra=array(),$extrarep=array()){
-	global $session, $badguy;
-	
-	$where=($forest?"WHERE forest=1":"WHERE graveyard=1");
+function select_deathmessage_array($forest=true,$extra=array(),$extrarep=array()){  
+global $session, $badguy;  
 
-	$sql = "SELECT deathmessage,taunt FROM " . db_prefix("deathmessages") .
-		" $where ORDER BY rand(".e_rand() . ") LIMIT 1";
+$where=($forest?"WHERE forest=1":"WHERE graveyard=1");  
 
-	$result = db_query($sql);
-	if ($result) {
-		$row = db_fetch_assoc($result);
-		$deathmessage = $row['deathmessage'];
-		$taunt=$row['taunt'];
-	} else {
-		$taunt=1;
-		$deathmessage = "`5\"`6{goodguyname}'s mother wears combat boots`5\", screams {badguyname}.";
-	}
-	if ($extra[0]=='{where}') $deathmessage=str_replace($extra[0],$extrarep[0],$deathmessage);
-	$deathmessage = substitute_array($deathmessage,$extra,$extrarep);
-	array_unshift($deathmessage, true, "deathmessages");
-	return array("deathmessage"=>$deathmessage,"taunt"=>$taunt);
+$sql = "SELECT deathmessage,taunt FROM " . db_prefix("deathmessages") .  
+	" $where ORDER BY rand(".e_rand() . ") LIMIT 1";  
+
+$result = db_query($sql);  
+if ($result) {  
+	$row = db_fetch_assoc($result);  
+	$deathmessage = $row['deathmessage'];  
+	$taunt=$row['taunt'];  
+} else {  
+	$taunt=1;  
+	$deathmessage = "`5\"`6{goodguyname}'s mother wears combat boots`5\", screams {badguyname}.";  
+}  
+
+// Check if $extra[0] and $extrarep[0] are set  
+if (isset($extra[0]) && $extra[0] == '{where}') {  
+	$deathmessage = str_replace($extra[0], isset($extrarep[0]) ? $extrarep[0] : "UNKNOWN", $deathmessage);  
+}  
+
+$deathmessage = substitute_array($deathmessage,$extra,$extrarep);  
+array_unshift($deathmessage, true, "deathmessages");  
+return array("deathmessage"=>$deathmessage,"taunt"=>$taunt);  
+}
 }
 ?>
