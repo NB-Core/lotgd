359475f98af8d0d24448553038d85625e5f221cb
Oliver <admin@shinobilegends.com>
2025-07-04 23:49:10 +0200

    debug change in sql statements

--- debug.php
+++ debug.php
@@ -36,7 +36,22 @@ addnav("Switch ASC/DESC","debug.php?debug=".$debug."&sort=".URLEncode($sort)."&d
 switch ($debug) {
 
 	case "hooksort":
-		$sql = "Select category,subcategory, value+0 as sum, (value+0)/count(id) as medium,count(id) AS counter FROM ".db_prefix('debug')." WHERE type='hooktime' group by type,category,subcategory order by $order $ascdesc limit 30";
+		$sql = "SELECT 
+					category, 
+					subcategory, 
+					SUM(value + 0) AS sum,
+					AVG(value + 0) AS medium,
+					COUNT(id) AS counter
+				FROM 
+					`".db_prefix('debug')."` 
+				WHERE 
+					type = 'hooktime'
+				GROUP BY 
+					category, subcategory
+				ORDER BY 
+					$order $ascdesc
+				LIMIT 30;
+				";
 		$category=translate_inline("Setting");
 		$subcategory=translate_inline("Module Name");
 		$sum_desc=translate_inline("Total Seconds");
@@ -47,7 +62,19 @@ switch ($debug) {
 	case "pageruntime":
 
 	default:
-	$sql = "Select category,subcategory, value+0 as sum, (value+0)/count(id) as medium,count(id) AS counter FROM ".db_prefix('debug')." WHERE type='pagegentime' group by type,category,subcategory order by $order $ascdesc limit 30";
+	$sql = "SELECT 
+				type,
+				category, 
+				subcategory, 
+				SUM(value + 0) AS sum,
+				AVG(value + 0) AS medium,
+				COUNT(id) AS counter
+			FROM ".db_prefix('debug')."
+			WHERE type = 'pagegentime'
+			GROUP BY type, category, subcategory
+			ORDER BY $order $ascdesc
+			LIMIT 30";
+
 	$category=translate_inline("Setting");
 	$subcategory=translate_inline("Module Name");
 	$sum_desc=translate_inline("Total Seconds");
