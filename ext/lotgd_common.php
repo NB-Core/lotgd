6c39371eccbe378239c1381a40426465d1461399
NB-Core <mourne@live.de>
2025-07-08 11:46:30 +0200

    Reload user data for AJAX

--- ext/lotgd_common.php
+++ ext/lotgd_common.php
@@ -7,4 +7,8 @@ if (!defined('AJAX_MODE')) {
 $cwd = getcwd();
 chdir(__DIR__ . '/../');
 require_once __DIR__ . '/../common.php';
+
+// Reload the player account and allowed navigation like a normal page
+\Lotgd\ForcedNavigation::doForcedNav(ALLOW_ANONYMOUS, OVERRIDE_FORCED_NAV);
+
 chdir($cwd);
