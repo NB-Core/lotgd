c187f1971a9e377bdc31849849e5e204851220f4
NB-Core <mourne@live.de>
2025-07-24 22:21:30 +0200

    Remove manual Symfony cache autoload

--- autoload.php
+++ autoload.php
@@ -7,5 +7,3 @@ if (!file_exists($autoloadPath)) {
 $loader = require $autoloadPath;
 $loader->addPsr4('Lotgd\\', __DIR__ . '/src/Lotgd/');
 $loader->addPsr4('Lotgd\\Installer\\', __DIR__ . '/install/lib/');
-$loader->addPsr4('Symfony\\Component\\Cache\\', __DIR__ . '/vendor/symfony/cache/');
-$loader->addPsr4('Symfony\\Contracts\\Cache\\', __DIR__ . '/vendor/symfony/cache-contracts/');
