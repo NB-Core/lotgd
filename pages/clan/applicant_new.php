1c8c445877ace6e267b5ec7ff7b69e93ba658f9d
NB-Core <mourne@live.de>
2025-07-28 19:37:53 +0200

    Fix Output handling in clan pages

--- pages/clan/applicant_new.php
+++ pages/clan/applicant_new.php
@@ -1,115 +1,124 @@
 <?php
+declare(strict_types=1);
 
-        $apply = httpget('apply');
+use Lotgd\Http;
+use Lotgd\Nav;
+use Lotgd\MySQL\Database;
+use Lotgd\Translator;
+use Lotgd\Modules;
+use Lotgd\Sanitize;
+use Lotgd\DebugLog;
+
+        $apply = Http::get('apply');
 if ($apply == 1) {
-    $ocn = httppost('clanname');
-    $ocs = httppost('clanshort');
+    $ocn = Http::post('clanname');
+    $ocs = Http::post('clanshort');
     $clanname = stripslashes($ocn);
-    $clanname = full_sanitize($clanname);
+    $clanname = Sanitize::fullSanitize($clanname);
     if (getsetting('clannamesanitize', 0)) {
         $clanname = preg_replace("'[^[:alpha:] \\'-]'", "", $clanname);
     }
     $clanname = addslashes($clanname);
-    httppostset('clanname', $clanname);
-    $clanshort = full_sanitize($ocs);
+    Http::postSet('clanname', $clanname);
+    $clanshort = Sanitize::fullSanitize($ocs);
     if (getsetting('clanshortnamesanitize', 0)) {
         $clanshort = preg_replace("'[^[:alpha:]]'", "", $clanshort);
     }
-    httppostset('clanshort', $clanshort);
-    $sql = "SELECT * FROM " . db_prefix("clans") . " WHERE clanname='$clanname'";
-    $result = db_query($sql);
-    $e = array (translate_inline("%s`7 looks over your form but informs you that your clan name must consist only of letters, spaces, apostrophes, or dashes.  Also, your short name can consist only of letters. She hands you a blank form."),
-        translate_inline("%s`7 looks over your form but informs you that you must have at least 5 and no more than 50 characters in your clan's name (and they must consist only of letters, spaces, apostrophes, or dashes), then hands you a blank form."),
-        translate_inline("%s`7 looks over your form but informs you that you must have at least 2 and no more than %s characters in your clan's short name (and they must all be letters), then hands you a blank form."),
-        translate_inline("%s`7 looks over your form but informs you that the clan name %s is already taken, and hands you a blank form."),
-        translate_inline("%s`7 looks over your form but informs you that the short name %s is already taken, and hands you a blank form."),
-        translate_inline("%s`7 asks for the %s gold to start the clan, but you seem to be unable to produce the fees."),
-        translate_inline("%s`7 asks for the %s gold and %s gems to start the clan, but you seem to be unable to produce the fees."),
-        translate_inline("%s`7 asks for the %s gems to start the clan, but you seem to be unable to produce the fees."),
-        translate_inline("She takes your application, and stamps it \"`\$DENIED`7\"."),
+    Http::postSet('clanshort', $clanshort);
+    $sql = "SELECT * FROM " . Database::prefix("clans") . " WHERE clanname='$clanname'";
+    $result = Database::query($sql);
+    $e = array (Translator::translateInline("%s`7 looks over your form but informs you that your clan name must consist only of letters, spaces, apostrophes, or dashes.  Also, your short name can consist only of letters. She hands you a blank form."),
+        Translator::translateInline("%s`7 looks over your form but informs you that you must have at least 5 and no more than 50 characters in your clan's name (and they must consist only of letters, spaces, apostrophes, or dashes), then hands you a blank form."),
+        Translator::translateInline("%s`7 looks over your form but informs you that you must have at least 2 and no more than %s characters in your clan's short name (and they must all be letters), then hands you a blank form."),
+        Translator::translateInline("%s`7 looks over your form but informs you that the clan name %s is already taken, and hands you a blank form."),
+        Translator::translateInline("%s`7 looks over your form but informs you that the short name %s is already taken, and hands you a blank form."),
+        Translator::translateInline("%s`7 asks for the %s gold to start the clan, but you seem to be unable to produce the fees."),
+        Translator::translateInline("%s`7 asks for the %s gold and %s gems to start the clan, but you seem to be unable to produce the fees."),
+        Translator::translateInline("%s`7 asks for the %s gems to start the clan, but you seem to be unable to produce the fees."),
+        Translator::translateInline("She takes your application, and stamps it \"`\$DENIED`7\"."),
     );
     if ($clanname != $ocn || $clanshort != $ocs) {
-        output_notl($e[0], $registrar);
+        $output->outputNotl($e[0], $registrar);
         clanform();
-        addnav("Return to the Lobby", "clan.php");
+        Nav::add("Return to the Lobby", "clan.php");
     } elseif (strlen($clanname) < 5 || strlen($clanname) > 50) {
-        output_notl($e[1], $registrar);
+        $output->outputNotl($e[1], $registrar);
         clanform();
-        addnav("Return to the Lobby", "clan.php");
+        Nav::add("Return to the Lobby", "clan.php");
     } elseif (strlen($clanshort) < 2 || strlen($clanshort) > getsetting('clanshortnamelength', 5)) {
-        output_notl($e[2], $registrar, getsetting('clanshortnamelength', 5));
+        $output->outputNotl($e[2], $registrar, getsetting('clanshortnamelength', 5));
         clanform();
-        addnav("Return to the Lobby", "clan.php");
-    } elseif (db_num_rows($result) > 0) {
-        output_notl($e[3], $registrar, stripslashes($clanname));
+        Nav::add("Return to the Lobby", "clan.php");
+    } elseif (Database::numRows($result) > 0) {
+        $output->outputNotl($e[3], $registrar, stripslashes($clanname));
         clanform();
-        addnav("Return to the Lobby", "clan.php");
+        Nav::add("Return to the Lobby", "clan.php");
     } else {
         //too many stupids put < and > in their clanshort name -_-
         $clanshort = str_replace("<", "", $clanshort);
         $clanshort = str_replace(">", "", $clanshort);
-        $sql = "SELECT * FROM " . db_prefix("clans") . " WHERE clanshort='$clanshort'";
-        $result = db_query($sql);
-        if (db_num_rows($result) > 0) {
-            output_notl($e[4], $registrar, stripslashes($clanshort));
+        $sql = "SELECT * FROM " . Database::prefix("clans") . " WHERE clanshort='$clanshort'";
+        $result = Database::query($sql);
+        if (Database::numRows($result) > 0) {
+            $output->outputNotl($e[4], $registrar, stripslashes($clanshort));
             clanform();
-            addnav("Return to the Lobby", "clan.php");
+            Nav::add("Return to the Lobby", "clan.php");
         } else {
             if ($session['user']['gold'] < $gold || $session['user']['gems'] < $gems) {
                 if ($gold > 0 && $gems <= 0) {
-                    output_notl($e[5], $registrar, $gold);
+                    $output->outputNotl($e[5], $registrar, $gold);
                 } elseif ($gems > 0 && $gold <= 0) {
-                    output_notl($e[7], $registrar, $gems);
+                    $output->outputNotl($e[7], $registrar, $gems);
                 } else {
-                    output_notl($e[6], $registrar, $gold, $gems);
+                    $output->outputNotl($e[6], $registrar, $gold, $gems);
                 }
-                output_notl($e[8], $registrar);
-                addnav("Return to the Lobby", "clan.php");
+                $output->outputNotl($e[8], $registrar);
+                Nav::add("Return to the Lobby", "clan.php");
             } else {
 /*//*/                      $args = array("ocn" => $ocn, "ocs" => $ocs, "clanname" => $clanname, "clanshort" => $clanshort);
-/*//*/                      $args = modulehook("process-createclan", $args);
+/*//*/                      $args = Modules::hook("process-createclan", $args);
 /*//*/                      if (isset($args['blocked']) && $args['blocked']) {
-/*//*/                          output_notl(sprintf_translate($args['blockmsg']));
+/*//*/                          $output->outputNotl(sprintf_translate($args['blockmsg']));
 /*//*/                          clanform();
-/*//*/                          addnav("Return to the Lobby", "clan.php");
+/*//*/                          Nav::add("Return to the Lobby", "clan.php");
 /*//*/
 } else {
-                            $sql = "INSERT INTO " . db_prefix("clans") . " (clanname,clanshort) VALUES ('$clanname','$clanshort')";
-                            db_query($sql);
-                            $id = db_insert_id();
+                            $sql = "INSERT INTO " . Database::prefix("clans") . " (clanname,clanshort) VALUES ('$clanname','$clanshort')";
+                            Database::query($sql);
+                            $id = Database::insertId();
                             $session['user']['clanid'] = $id;
                             $session['user']['clanrank'] = CLAN_LEADER + 1; //+1 because he is the founder
                             $session['user']['clanjoindate'] = date("Y-m-d H:i:s");
                             $session['user']['gold'] -= $gold;
                             $session['user']['gems'] -= $gems;
-                            debuglog("has started a new clan (<$clanshort> $clanname) for $gold gold and $gems gems.");
-                            output("%s`7 looks over your form, and finding that everything seems to be in order, she takes your fees, stamps the form \"`\$APPROVED`7\" and files it in a drawer.`n`n", $registrar);
-                            output("Congratulations, you've created a new clan named %s!", stripslashes($clanname));
-                            addnav("Enter your clan hall", "clan.php");
+                            DebugLog::add("has started a new clan (<$clanshort> $clanname) for $gold gold and $gems gems.");
+                            $output->output("%s`7 looks over your form, and finding that everything seems to be in order, she takes your fees, stamps the form \"`\$APPROVED`7\" and files it in a drawer.`n`n", $registrar);
+                            $output->output("Congratulations, you've created a new clan named %s!", stripslashes($clanname));
+                            Nav::add("Enter your clan hall", "clan.php");
 /*//*/
 }
             }
         }
     }
 } else {
-    output("`7You approach %s`7 and inquire about starting a new clan.", $registrar);
-    output("She tells you that there are three requirements to starting a clan.");
-    output("First, you have to decide on a full name for your clan.");
-    output("Second, you have to decide on an abbreviation for your clan.");
-    output("Third you have to decide on whether or not you're willing to give up the fees that are required to start the clan.");
-    output("This fee is used to tailor the locks on your clan door to you and your members.`n");
-    output("The fees are as follows:`nGold: `^%s`7`nGems: `%%s`7", $gold, $gems);
-    addnav("Return to the Lobby", "clan.php");
-    $e1 = translate_inline("`n`n\"`5Since you do not have enough gold with you, I cannot allow you to apply for a clan,`7\" she says.");
-    $e2 = translate_inline("`n`n\"`5Since you do not have enough gems with you, I cannot allow you to apply for a clan,`7\" she says.");
-    $e3 = translate_inline("`n`n\"`5If you're ok with these three requirements, please fill out the following form,`7\" she says, handing you a sheet of paper.");
+    $output->output("`7You approach %s`7 and inquire about starting a new clan.", $registrar);
+    $output->output("She tells you that there are three requirements to starting a clan.");
+    $output->output("First, you have to decide on a full name for your clan.");
+    $output->output("Second, you have to decide on an abbreviation for your clan.");
+    $output->output("Third you have to decide on whether or not you're willing to give up the fees that are required to start the clan.");
+    $output->output("This fee is used to tailor the locks on your clan door to you and your members.`n");
+    $output->output("The fees are as follows:`nGold: `^%s`7`nGems: `%%s`7", $gold, $gems);
+    Nav::add("Return to the Lobby", "clan.php");
+    $e1 = Translator::translateInline("`n`n\"`5Since you do not have enough gold with you, I cannot allow you to apply for a clan,`7\" she says.");
+    $e2 = Translator::translateInline("`n`n\"`5Since you do not have enough gems with you, I cannot allow you to apply for a clan,`7\" she says.");
+    $e3 = Translator::translateInline("`n`n\"`5If you're ok with these three requirements, please fill out the following form,`7\" she says, handing you a sheet of paper.");
     if ($session['user']['gold'] < $gold) {
-        output_notl($e1);
+        $output->outputNotl($e1);
     } else {
         if ($session['user']['gems'] < $gems) {
-            output_notl($e2, $registrar);
+            $output->outputNotl($e2, $registrar);
         } else {
-            output_notl($e3, $registrar);
+            $output->outputNotl($e3, $registrar);
             clanform();
         }
     }
--- pages/clan/detail.php
+++ pages/clan/detail.php
@@ -1,102 +1,112 @@
 <?php
+declare(strict_types=1);
+
+use Lotgd\Http;
+use Lotgd\Sanitize;
+use Lotgd\MySQL\Database;
+use Lotgd\DataCache;
+use Lotgd\Translator;
+use Lotgd\Nav;
+use Lotgd\Modules;
+use Lotgd\Nltoappon;
 
 if ($session['user']['superuser'] & SU_EDIT_COMMENTS) {
-    $clanname = httppost('clanname');
+    $clanname = Http::post('clanname');
     if ($clanname) {
-        $clanname = full_sanitize($clanname);
+        $clanname = Sanitize::fullSanitize($clanname);
     }
-    $clanshort = httppost('clanshort');
+    $clanshort = Http::post('clanshort');
     if ($clanshort) {
-        $clanshort = full_sanitize($clanshort);
+        $clanshort = Sanitize::fullSanitize($clanshort);
     }
     if ($clanname > "" && $clanshort > "") {
-        $sql = "UPDATE " . db_prefix("clans") . " SET clanname='$clanname',clanshort='$clanshort' WHERE clanid='$detail'";
-        output("Updating clan names`n");
-        db_query($sql);
-        invalidatedatacache("clandata-$detail");
+        $sql = "UPDATE " . Database::prefix("clans") . " SET clanname='$clanname',clanshort='$clanshort' WHERE clanid='$detail'";
+        $output->output("Updating clan names`n");
+        Database::query($sql);
+        DataCache::invalidatedatacache("clandata-$detail");
     }
-    if (httppost('block') > "") {
-        $blockdesc = translate_inline("Description blocked for inappropriate usage.");
-        $sql = "UPDATE " . db_prefix("clans") . " SET descauthor=4294967295, clandesc='$blockdesc' where clanid='$detail'";
-        output("Blocking public description`n");
-        db_query($sql);
-        invalidatedatacache("clandata-$detail");
-    } elseif (httppost('unblock') > "") {
-        $sql = "UPDATE " . db_prefix("clans") . " SET descauthor=0, clandesc='' where clanid='$detail'";
-        output("UNblocking public description`n");
-        db_query($sql);
-        invalidatedatacache("clandata-$detail");
+    if (Http::post('block') > "") {
+        $blockdesc = Translator::translateInline("Description blocked for inappropriate usage.");
+        $sql = "UPDATE " . Database::prefix("clans") . " SET descauthor=4294967295, clandesc='$blockdesc' where clanid='$detail'";
+        $output->output("Blocking public description`n");
+        Database::query($sql);
+        DataCache::invalidatedatacache("clandata-$detail");
+    } elseif (Http::post('unblock') > "") {
+        $sql = "UPDATE " . Database::prefix("clans") . " SET descauthor=0, clandesc='' where clanid='$detail'";
+        $output->output("UNblocking public description`n");
+        Database::query($sql);
+        DataCache::invalidatedatacache("clandata-$detail");
     }
 }
-    $sql = "SELECT * FROM " . db_prefix("clans") . " WHERE clanid='$detail'";
-    $result1 = db_query_cached($sql, "clandata-$detail", 3600);
-    $row1 = db_fetch_assoc($result1);
+    $sql = "SELECT * FROM " . Database::prefix("clans") . " WHERE clanid='$detail'";
+    $result1 = Database::queryCached($sql, "clandata-$detail", 3600);
+    $row1 = Database::fetchAssoc($result1);
 if ($session['user']['superuser'] & SU_AUDIT_MODERATION) {
-    rawoutput("<div id='hidearea'>");
-    rawoutput("<form action='clan.php?detail=$detail' method='POST'>");
-    addnav("", "clan.php?detail=$detail");
-    output("Superuser / Moderator renaming:`n");
-    output("Long Name: ");
-    rawoutput("<input name='clanname' value=\"" . htmlentities($row1['clanname'], ENT_COMPAT, getsetting("charset", "ISO-8859-1")) . "\" maxlength=50 size=50>");
-    output("`nShort Name: ");
-    rawoutput("<input name='clanshort' value=\"" . htmlentities($row1['clanshort'], ENT_COMPAT, getsetting("charset", "ISO-8859-1")) . "\" maxlength=5 size=5>");
-    output_notl("`n");
-    $save = translate_inline("Save");
-    rawoutput("<input type='submit' class='button' value=\"$save\">");
-    $snu = htmlentities(translate_inline("Save & UNblock public description"), ENT_COMPAT, getsetting("charset", "ISO-8859-1"));
-    $snb = htmlentities(translate_inline("Save & Block public description"), ENT_COMPAT, getsetting("charset", "ISO-8859-1"));
+    $output->rawOutput("<div id='hidearea'>");
+    $output->rawOutput("<form action='clan.php?detail=$detail' method='POST'>");
+    Nav::add("", "clan.php?detail=$detail");
+    $output->output("Superuser / Moderator renaming:`n");
+    $output->output("Long Name: ");
+    $output->rawOutput("<input name='clanname' value=\"" . htmlentities($row1['clanname'], ENT_COMPAT, getsetting("charset", "ISO-8859-1")) . "\" maxlength=50 size=50>");
+    $output->output("`nShort Name: ");
+    $output->rawOutput("<input name='clanshort' value=\"" . htmlentities($row1['clanshort'], ENT_COMPAT, getsetting("charset", "ISO-8859-1")) . "\" maxlength=5 size=5>");
+    $output->outputNotl("`n");
+    $save = Translator::translateInline("Save");
+    $output->rawOutput("<input type='submit' class='button' value=\"$save\">");
+    $snu = htmlentities(Translator::translateInline("Save & UNblock public description"), ENT_COMPAT, getsetting("charset", "ISO-8859-1"));
+    $snb = htmlentities(Translator::translateInline("Save & Block public description"), ENT_COMPAT, getsetting("charset", "ISO-8859-1"));
     if ($row1['descauthor'] == "4294967295") {
-        rawoutput("<input type='submit' name='unblock' value=\"$snu\" class='button'>");
+        $output->rawOutput("<input type='submit' name='unblock' value=\"$snu\" class='button'>");
     } else {
-        rawoutput("<input type='submit' name='block' value=\"$snb\" class='button'>");
+        $output->rawOutput("<input type='submit' name='block' value=\"$snb\" class='button'>");
     }
-    rawoutput("</form>");
-    rawoutput("</div>");
-    rawoutput("<script language='JavaScript'>var hidearea = document.getElementById('hidearea');hidearea.style.visibility='hidden';hidearea.style.display='none';</script>", true);
-    $e = translate_inline("Edit Clan Info");
-    rawoutput("<a href='#' onClick='hidearea.style.visibility=\"visible\"; hidearea.style.display=\"inline\"; return false;'>$e</a>", true);
-    output_notl("`n");
+    $output->rawOutput("</form>");
+    $output->rawOutput("</div>");
+    $output->rawOutput("<script language='JavaScript'>var hidearea = document.getElementById('hidearea');hidearea.style.visibility='hidden';hidearea.style.display='none';</script>", true);
+    $e = Translator::translateInline("Edit Clan Info");
+    $output->rawOutput("<a href='#' onClick='hidearea.style.visibility=\"visible\"; hidearea.style.display=\"inline\"; return false;'>$e</a>", true);
+    $output->outputNotl("`n");
 }
 
-    output_notl(nltoappon($row1['clandesc']));
-if (nltoappon($row1['clandesc']) != "") {
-    output("`n`n");
+    $output->outputNotl(Nltoappon::convert($row1['clandesc']));
+if (Nltoappon::convert($row1['clandesc']) != "") {
+    $output->output("`n`n");
 }
-    output("`0This is the current clan membership of %s < %s >:`n", $row1['clanname'], $row1['clanshort']);
-    page_header("Clan Membership for %s &lt;%s&gt;", full_sanitize($row1['clanname']), full_sanitize($row1['clanshort']));
-    addnav("Clan Options");
-    $rank = translate_inline("Rank");
-    $name = translate_inline("Name");
-    $dk = translate_inline("Dragon Kills");
-    $jd = translate_inline("Join Date");
-    rawoutput("<table border='0' cellpadding='2' cellspacing='0'>");
-    rawoutput("<tr class='trhead'><td>$rank</td><td>$name</td><td>$dk</td><td>$jd</td></tr>");
+    $output->output("`0This is the current clan membership of %s < %s >:`n", $row1['clanname'], $row1['clanshort']);
+    page_header("Clan Membership for %s &lt;%s&gt;", Sanitize::fullSanitize($row1['clanname']), Sanitize::fullSanitize($row1['clanshort']));
+    Nav::add("Clan Options");
+    $rank = Translator::translateInline("Rank");
+    $name = Translator::translateInline("Name");
+    $dk = Translator::translateInline("Dragon Kills");
+    $jd = Translator::translateInline("Join Date");
+    $output->rawOutput("<table border='0' cellpadding='2' cellspacing='0'>");
+    $output->rawOutput("<tr class='trhead'><td>$rank</td><td>$name</td><td>$dk</td><td>$jd</td></tr>");
     $i = 0;
-    $sql = "SELECT acctid,name,login,clanrank,clanjoindate,dragonkills FROM " . db_prefix("accounts") . " WHERE clanid=$detail ORDER BY clanrank DESC,clanjoindate";
-    $result = db_query($sql);
+    $sql = "SELECT acctid,name,login,clanrank,clanjoindate,dragonkills FROM " . Database::prefix("accounts") . " WHERE clanid=$detail ORDER BY clanrank DESC,clanjoindate";
+    $result = Database::query($sql);
     $tot = 0;
     //little hack with the hook...can't think of any other way
     $ranks = array(CLAN_APPLICANT => "`!Applicant`0",CLAN_MEMBER => "`#Member`0",CLAN_OFFICER => "`^Officer`0",CLAN_LEADER => "`&Leader`0", CLAN_FOUNDER => "`\$Founder");
-    $args = modulehook("clanranks", array("ranks" => $ranks, "clanid" => $detail));
-    $ranks = translate_inline($args['ranks']);
+    $args = Modules::hook("clanranks", array("ranks" => $ranks, "clanid" => $detail));
+    $ranks = Translator::translateInline($args['ranks']);
     //end
-while ($row = db_fetch_assoc($result)) {
+while ($row = Database::fetchAssoc($result)) {
     $i++;
     $tot += $row['dragonkills'];
-    rawoutput("<tr class='" . ($i % 2 ? "trlight" : "trdark") . "'>");
-    rawoutput("<td>");
-    output_notl($ranks[$row['clanrank']]); //translated earlier
-    rawoutput("</td><td>");
+    $output->rawOutput("<tr class='" . ($i % 2 ? "trlight" : "trdark") . "'>");
+    $output->rawOutput("<td>");
+    $output->outputNotl($ranks[$row['clanrank']]); //translated earlier
+    $output->rawOutput("</td><td>");
     $link = "bio.php?char=" . $row['acctid'] . "&ret=" . urlencode($_SERVER['REQUEST_URI']);
-    rawoutput("<a href='$link'>");
-    addnav("", $link);
-    output_notl("`&%s`0", $row['name']);
-    rawoutput("</a>");
-    rawoutput("</td><td align='center'>");
-    output_notl("`\$%s`0", $row['dragonkills']);
-    rawoutput("</td><td>");
-    output_notl("`3%s`0", $row['clanjoindate']);
-    rawoutput("</td></tr>");
+    $output->rawOutput("<a href='$link'>");
+    Nav::add("", $link);
+    $output->outputNotl("`&%s`0", $row['name']);
+    $output->rawOutput("</a>");
+    $output->rawOutput("</td><td align='center'>");
+    $output->outputNotl("`\$%s`0", $row['dragonkills']);
+    $output->rawOutput("</td><td>");
+    $output->outputNotl("`3%s`0", $row['clanjoindate']);
+    $output->rawOutput("</td></tr>");
 }
-    rawoutput("</table>");
-    output("`n`n`^This clan has a total of `\$%s`^ dragon kills.", $tot);
+    $output->rawOutput("</table>");
+    $output->output("`n`n`^This clan has a total of `\$%s`^ dragon kills.", $tot);
