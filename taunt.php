a2763fe47088f6d367b7f73f52d5394348414576
NB-Core <mourne@live.de>
2025-07-28 21:16:35 +0200

    Migrate taunt and death message editors

--- taunt.php
+++ taunt.php
@@ -1,98 +1,105 @@
 <?php
+declare(strict_types=1);
 
 use Lotgd\SuAccess;
 use Lotgd\Nav\SuperuserNav;
 use Lotgd\Substitute;
+use Lotgd\Http;
+use Lotgd\Page\Header;
+use Lotgd\Page\Footer;
+use Lotgd\Nav;
+use Lotgd\Translator;
+use Lotgd\MySQL\Database;
+use Lotgd\Settings;
 
 // addnews ready
 // mail ready
 // translator ready
 require_once("common.php");
-require_once("lib/http.php");
 
-tlschema("taunt");
+Translator::tlschema("taunt");
 
 SuAccess::check(SU_EDIT_CREATURES);
 
-page_header("Taunt Editor");
+Header::pageHeader("Taunt Editor");
 SuperuserNav::render();
-$op = httpget('op');
-$tauntid = httpget('tauntid');
+$op = Http::get('op');
+$tauntid = Http::get('tauntid');
 if ($op == "edit") {
-    addnav("Taunts");
-    addnav("Return to the taunt editor", "taunt.php");
-    rawoutput("<form action='taunt.php?op=save&tauntid=$tauntid' method='POST'>", true);
-    addnav("", "taunt.php?op=save&tauntid=$tauntid");
+    Nav::add("Taunts");
+    Nav::add("Return to the taunt editor", "taunt.php");
+    $output->rawOutput("<form action='taunt.php?op=save&tauntid=$tauntid' method='POST'>", true);
+    Nav::add("", "taunt.php?op=save&tauntid=$tauntid");
     if ($tauntid != "") {
-        $sql = "SELECT * FROM " . db_prefix("taunts") . " WHERE tauntid=\"$tauntid\"";
-        $result = db_query($sql);
-        $row = db_fetch_assoc($result);
+        $sql = "SELECT * FROM " . Database::prefix("taunts") . " WHERE tauntid=\"$tauntid\"";
+        $result = Database::query($sql);
+        $row = Database::fetchAssoc($result);
         $badguy = array('creaturename' => 'Baron Munchausen', 'creatureweapon' => 'Bad Puns');
         $taunt = Substitute::applyArray($row['taunt']);
-        $taunt = call_user_func_array("sprintf_translate", $taunt);
-        output("Preview: %s`0`n`n", $taunt);
+        $taunt = Translator::sprintfTranslate(...$taunt);
+        $output->output("Preview: %s`0`n`n", $taunt);
     } else {
         $row = array('tauntid' => 0, 'taunt' => "");
     }
-    output("Taunt: ");
-    rawoutput("<input name='taunt' value=\"" . HTMLEntities($row['taunt'], ENT_COMPAT, getsetting("charset", "ISO-8859-1")) . "\" size='70'><br>");
-    output("The following codes are supported (case matters):`n");
-    output("{goodguyname}	= The player's name (also can be specified as {goodguy}`n");
-    output("{goodguyweapon}	= The player's weapon (also can be specified as {weapon}`n");
-    output("{armorname}	= The player's armor (also can be specified as {armor}`n");
-    output("{himher}	= Subjective pronoun for the player (him her)`n");
-    output("{hisher}	= Possessive pronoun for the player (his her)`n");
-    output("{heshe}		= Objective pronoun for the player (he she)`n");
-    output("{badguyname}	= The monster's name (also can be specified as {badguy}`n");
-    output("{badguyweapon}	= The monster's weapon (also can be specified as {creatureweapon}`n");
-    $save = translate_inline("Save");
-    rawoutput("<input type='submit' class='button' value='$save'>");
-    rawoutput("</form>");
+    $output->output("Taunt: ");
+    $output->rawOutput("<input name='taunt' value=\"" . HTMLEntities($row['taunt'], ENT_COMPAT, Settings::getsetting("charset", "ISO-8859-1")) . "\" size='70'><br>");
+    $output->output("The following codes are supported (case matters):`n");
+    $output->output("{goodguyname}	= The player's name (also can be specified as {goodguy}`n");
+    $output->output("{goodguyweapon}	= The player's weapon (also can be specified as {weapon}`n");
+    $output->output("{armorname}	= The player's armor (also can be specified as {armor}`n");
+    $output->output("{himher}	= Subjective pronoun for the player (him her)`n");
+    $output->output("{hisher}	= Possessive pronoun for the player (his her)`n");
+    $output->output("{heshe}		= Objective pronoun for the player (he she)`n");
+    $output->output("{badguyname}	= The monster's name (also can be specified as {badguy}`n");
+    $output->output("{badguyweapon}	= The monster's weapon (also can be specified as {creatureweapon}`n");
+    $save = Translator::translateInline("Save");
+    $output->rawOutput("<input type='submit' class='button' value='$save'>");
+    $output->rawOutput("</form>");
 } elseif ($op == "del") {
-    $sql = "DELETE FROM " . db_prefix("taunts") . " WHERE tauntid=\"$tauntid\"";
-    db_query($sql);
+    $sql = "DELETE FROM " . Database::prefix("taunts") . " WHERE tauntid=\"$tauntid\"";
+    Database::query($sql);
     $op = "";
-    httpset("op", "");
+    Http::set("op", "");
 } elseif ($op == "save") {
-    $taunt = httppost('taunt');
+    $taunt = Http::post('taunt');
     if ($tauntid != "") {
-        $sql = "UPDATE " . db_prefix("taunts") . " SET taunt=\"$taunt\",editor=\"" . addslashes($session['user']['login']) . "\" WHERE tauntid=\"$tauntid\"";
+        $sql = "UPDATE " . Database::prefix("taunts") . " SET taunt=\"$taunt\",editor=\"" . addslashes($session['user']['login']) . "\" WHERE tauntid=\"$tauntid\"";
     } else {
-        $sql = "INSERT INTO " . db_prefix("taunts") . " (taunt,editor) VALUES (\"$taunt\",\"" . addslashes($session['user']['login']) . "\")";
+        $sql = "INSERT INTO " . Database::prefix("taunts") . " (taunt,editor) VALUES (\"$taunt\",\"" . addslashes($session['user']['login']) . "\")";
     }
-    db_query($sql);
+    Database::query($sql);
     $op = "";
-    httpset("op", "");
+    Http::set("op", "");
 }
 if ($op == "") {
-    $sql = "SELECT * FROM " . db_prefix("taunts");
-    $result = db_query($sql);
-    rawoutput("<table border=0 cellpadding=2 cellspacing=1 bgcolor='#999999'>");
-    $op = translate_inline("Ops");
-    $t = translate_inline("Taunt String");
-    $auth = translate_inline("Author");
-    rawoutput("<tr class='trhead'><td nowrap>$op</td><td>$t</td><td>$auth</td></tr>");
-    $number = db_num_rows($result);
+    $sql = "SELECT * FROM " . Database::prefix("taunts");
+    $result = Database::query($sql);
+    $output->rawOutput("<table border=0 cellpadding=2 cellspacing=1 bgcolor='#999999'>");
+    $op = Translator::translateInline("Ops");
+    $t = Translator::translateInline("Taunt String");
+    $auth = Translator::translateInline("Author");
+    $output->rawOutput("<tr class='trhead'><td nowrap>$op</td><td>$t</td><td>$auth</td></tr>");
+    $number = Database::numRows($result);
     for ($i = 0; $i < $number; $i++) {
-        $row = db_fetch_assoc($result);
-        rawoutput("<tr class='" . ($i % 2 == 0 ? "trdark" : "trlight") . "'>", true);
-        rawoutput("<td nowrap>");
-        $edit = translate_inline("Edit");
-        $del = translate_inline("Del");
-        $conf = translate_inline("Are you sure you wish to delete this taunt?");
+        $row = Database::fetchAssoc($result);
+        $output->rawOutput("<tr class='" . ($i % 2 == 0 ? "trdark" : "trlight") . "'>", true);
+        $output->rawOutput("<td nowrap>");
+        $edit = Translator::translateInline("Edit");
+        $del = Translator::translateInline("Del");
+        $conf = Translator::translateInline("Are you sure you wish to delete this taunt?");
         $id = $row['tauntid'];
-        rawoutput("[ <a href='taunt.php?op=edit&tauntid=$id'>$edit</a> | <a href='taunt.php?op=del&tauntid=$id' onClick='return confirm(\"$conf\");'>$del</a> ]");
-        addnav("", "taunt.php?op=edit&tauntid=$id");
-        addnav("", "taunt.php?op=del&tauntid=$id");
-        rawoutput("</td><td>");
-        output_notl("%s", $row['taunt']);
-        rawoutput("</td><td>");
-        output_notl("%s", $row['editor']);
-        rawoutput("</td></tr>");
+        $output->rawOutput("[ <a href='taunt.php?op=edit&tauntid=$id'>$edit</a> | <a href='taunt.php?op=del&tauntid=$id' onClick='return confirm(\"$conf\");'>$del</a> ]");
+        Nav::add("", "taunt.php?op=edit&tauntid=$id");
+        Nav::add("", "taunt.php?op=del&tauntid=$id");
+        $output->rawOutput("</td><td>");
+        $output->outputNotl("%s", $row['taunt']);
+        $output->rawOutput("</td><td>");
+        $output->outputNotl("%s", $row['editor']);
+        $output->rawOutput("</td></tr>");
     }
-    addnav("", "taunt.php?c=" . httpget('c'));
-    rawoutput("</table>");
-    addnav("Taunts");
-    addnav("Add a new taunt", "taunt.php?op=edit");
+    Nav::add("", "taunt.php?c=" . Http::get('c'));
+    $output->rawOutput("</table>");
+    Nav::add("Taunts");
+    Nav::add("Add a new taunt", "taunt.php?op=edit");
 }
-page_footer();
+Footer::pageFooter();
