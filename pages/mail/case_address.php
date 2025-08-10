6954305ad4862eec011c9d2126456328e7712b06
NB-Core <mourne@live.de>
2025-08-10 10:55:56 +0200

    refactor mail address to function

--- pages/mail/case_address.php
+++ pages/mail/case_address.php
@@ -5,22 +5,42 @@ declare(strict_types=1);
 use Lotgd\Translator;
 use Lotgd\Http;
 
-global $output;
-
-$output->outputNotl("<form action='mail.php?op=write' method='post'>", true);
-$output->output("`b`2Address:`b`n");
-$to = Translator::translateInline("To: ");
-$forwardto = Translator::translateInline("Forward To: ");
-$search = htmlentities(Translator::translateInline("Search"), ENT_COMPAT, getsetting("charset", "ISO-8859-1"));
 $id = (int) Http::get('id');
-$forwardlink = '';
-if ($id > 0) {
-    $to = $forwardto;
-    $forwardlink = "<input type='hidden' name='forwardto' value='$id'>";
-}
 $preop = (string) Http::get('preop');
-$output->outputNotl("`2$to <input name='to' id='to' value=\"" . htmlentities($preop, ENT_COMPAT, getsetting("charset", "ISO-8859-1")) . "\">", true);
-$output->outputNotl("<input type='submit' class='button' value=\"$search\">", true);
-$output->rawOutput($forwardlink);
-$output->rawOutput("</form>");
-$output->rawOutput("<script type='text/javascript'>document.getElementById(\"to\").focus();</script>");
+
+/**
+ * Render the address form for mail.
+ *
+ * @param int    $id    Forward recipient ID if provided.
+ * @param string $preop Prepopulated value for "to" field.
+ */
+function mailAddress(int $id, string $preop): void
+{
+    global $output;
+
+    $output->outputNotl("<form action='mail.php?op=write' method='post'>", true);
+    $output->output("`b`2Address:`b`n");
+    $to = Translator::translateInline("To: ");
+    $forwardto = Translator::translateInline("Forward To: ");
+    $search = htmlentities(Translator::translateInline("Search"), ENT_COMPAT, getsetting("charset", "ISO-8859-1"));
+    $forwardlink = '';
+
+    if ($id > 0) {
+        $to = $forwardto;
+        $forwardlink = "<input type='hidden' name='forwardto' value='$id'>";
+    }
+
+    $output->outputNotl(
+        "`2$to <input name='to' id='to' value=\""
+        . htmlentities($preop, ENT_COMPAT, getsetting("charset", "ISO-8859-1"))
+        . "\">",
+        true
+    );
+    $output->outputNotl("<input type='submit' class='button' value=\"$search\">", true);
+    $output->rawOutput($forwardlink);
+    $output->rawOutput("</form>");
+    $output->rawOutput("<script type='text/javascript'>document.getElementById(\"to\").focus();</script>");
+}
+
+mailAddress($id, $preop);
+
