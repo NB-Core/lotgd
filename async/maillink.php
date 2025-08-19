4952b5461d57310b8d2b1506f81fccfee11bde98
NB-Core <mourne@live.de>
2025-08-19 12:48:32 +0200

    Guard AJAX polling start until Jaxon is ready

--- async/maillink.php
+++ async/maillink.php
@@ -18,6 +18,7 @@ global $jaxon, $session, $check_mail_timeout_seconds, $start_timeout_show_second
 $s_js = $jaxon->getJs();
 $s_script = $jaxon->getScript();
 $maillink_add_pre = $s_js . $s_script;
+$maillink_add_pre .= "<script src='/async/js/ajax_polling.js' defer></script>";
 $maillink_add_after = "<script>";
 $maillink_add_after .= "var lotgd_comment_section = " . json_encode($session['last_comment_section'] ?? '') . ";";
 $maillink_add_after .= "var lotgd_lastCommentId = " . (int)($session['lastcommentid'] ?? 0) . ";";
@@ -25,5 +26,4 @@ $maillink_add_after .= "var lotgd_poll_interval_ms = " . (($check_mail_timeout_s
 $maillink_add_after .= "var lotgd_timeout_delay_ms = " . ((getsetting('LOGINTIMEOUT', 900) - ($start_timeout_show_seconds ?? 300)) * 1000) . ";";
 $maillink_add_after .= "var lotgd_clear_delay_ms = " . ((getsetting('LOGINTIMEOUT', 900) - ($clear_script_execution_seconds ?? -1)) * 1000) . ";";
 $maillink_add_after .= "</script>";
-$maillink_add_after .= "<script src='/async/js/ajax_polling.js' defer></script>";
 $maillink_add_after .= "<div id='notify'></div>";
