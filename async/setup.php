85d0ed96531d334a9875f2aac944a4eb2a3eaab7
Oliver <admin@shinobilegends.com>
2025-08-20 13:05:05 +0200

    Commentary ... now

--- async/setup.php
+++ async/setup.php
@@ -35,7 +35,7 @@ $polling_script .= "var lotgd_lastCommentId = " . (int)($session['lastcommentid'
 $polling_script .= "var lotgd_poll_interval_ms = " . ($check_mail_timeout_seconds * 1000) . ";";
 $polling_script .= "var lotgd_timeout_delay_ms = " . ((getsetting('LOGINTIMEOUT', 900) - $start_timeout_show_seconds) * 1000) . ";";
 $polling_script .= "var lotgd_clear_delay_ms = " . ((getsetting('LOGINTIMEOUT', 900) - $clear_script_execution_seconds) * 1000) . ";";
-$polling_script .= "console.log('Polling variables set:', {poll_interval: lotgd_poll_interval_ms, comment_section: lotgd_comment_section});";
+$polling_script .= "console.log('Polling variables set:', {poll_interval: lotgd_poll_interval_ms, comment_section: lotgd_comment_section, lastCommentId: lotgd_lastCommentId});";
 
 // INLINE POLLING SOLUTION - since ajax_polling.js isn't loading properly
 $polling_script .= "
@@ -48,6 +48,8 @@ function startInlinePolling() {
     }
     
     console.log('INLINE: Starting polling with interval:', lotgd_poll_interval_ms);
+    console.log('INLINE: Comment section:', lotgd_comment_section, 'Last ID:', lotgd_lastCommentId);
+    
     if (active_poll_interval) {
         clearInterval(active_poll_interval);
     }
@@ -58,18 +60,37 @@ function startInlinePolling() {
             && Lotgd.Async.Handler.Commentary
             && typeof Lotgd.Async.Handler.Commentary.pollUpdates === 'function') {
             
-            console.log('INLINE: Calling pollUpdates');
-            Lotgd.Async.Handler.Commentary.pollUpdates(lotgd_comment_section || '', lotgd_lastCommentId || 0);
+            console.log('INLINE: Calling pollUpdates with section:', lotgd_comment_section, 'lastId:', lotgd_lastCommentId);
+            
+            // Call pollUpdates and track the response
+            var response = Lotgd.Async.Handler.Commentary.pollUpdates(lotgd_comment_section || '', lotgd_lastCommentId || 0);
+            console.log('INLINE: pollUpdates response:', response);
+            
         } else {
             console.log('INLINE: Handlers not ready yet');
         }
     }, lotgd_poll_interval_ms);
 }
 
+// Test individual commentary method
+function testCommentaryRefresh() {
+    if (typeof Lotgd !== 'undefined' 
+        && Lotgd.Async && Lotgd.Async.Handler 
+        && Lotgd.Async.Handler.Commentary
+        && typeof Lotgd.Async.Handler.Commentary.commentaryRefresh === 'function') {
+        
+        console.log('INLINE: Testing commentaryRefresh directly...');
+        Lotgd.Async.Handler.Commentary.commentaryRefresh(lotgd_comment_section || '', lotgd_lastCommentId || 0);
+    }
+}
+
 // Start polling after a short delay
 setTimeout(function() {
     console.log('INLINE: Starting inline polling...');
     startInlinePolling();
+    
+    // Also test commentary refresh separately after 5 seconds
+    setTimeout(testCommentaryRefresh, 5000);
 }, 3000);
 ";
 
