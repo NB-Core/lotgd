72aa27d6f4c5b082ec2ef38cbf87f0cc5b77d5e8
Oliver <admin@shinobilegends.com>
2025-08-20 12:06:29 +0200

    Next fix...

--- async/setup.php
+++ async/setup.php
@@ -25,9 +25,21 @@ $pre_headscript = ($pre_headscript ?? '')
 $pre_headscript .= "<script>" . file_get_contents(__DIR__ . '/js/lotgd.jaxon.js') . "</script>"
     . $s_script;
 
-// Include maillink variables for polling - this was missing!
-require_once __DIR__ . '/maillink.php';
-$pre_headscript .= $maillink_add_after;
+// Add polling variables directly here instead of using maillink.php
+// Load the async settings
+require_once __DIR__ . '/common/settings.php';
+
+$polling_script = "<script>";
+$polling_script .= "var lotgd_comment_section = " . json_encode($session['last_comment_section'] ?? '') . ";";
+$polling_script .= "var lotgd_lastCommentId = " . (int)($session['lastcommentid'] ?? 0) . ";";
+$polling_script .= "var lotgd_poll_interval_ms = " . ($check_mail_timeout_seconds * 1000) . ";";
+$polling_script .= "var lotgd_timeout_delay_ms = " . ((getsetting('LOGINTIMEOUT', 900) - $start_timeout_show_seconds) * 1000) . ";";
+$polling_script .= "var lotgd_clear_delay_ms = " . ((getsetting('LOGINTIMEOUT', 900) - $clear_script_execution_seconds) * 1000) . ";";
+$polling_script .= "console.log('Polling variables set:', {poll_interval: lotgd_poll_interval_ms, comment_section: lotgd_comment_section});";
+$polling_script .= "</script>";
+$polling_script .= "<div id='notify'></div>";
+
+$pre_headscript .= $polling_script;
 
 // Add remaining scripts
 $pre_headscript .= "<script src='/async/js/jquery.min.js' defer></script>"
