93c9ba9c86e2babc5073e8b6b6c060785f9982b4
NB-Core <mourne@live.de>
2025-08-27 18:43:40 +0200

    test: verify cron exception is logged

--- tests/CronCommonExceptionTest.php
+++ tests/CronCommonExceptionTest.php
@@ -8,11 +8,21 @@ use PHPUnit\Framework\TestCase;
 
 final class CronCommonExceptionTest extends TestCase
 {
-    public function testExceptionInCommonTriggersEmail(): void
+    public function testExceptionInCommonIsLogged(): void
     {
-        $file = tempnam(sys_get_temp_dir(), 'cron');
-        shell_exec('php ' . escapeshellarg(__DIR__ . '/cron_common_exception.php') . ' ' . escapeshellarg($file));
-        $this->assertSame('1', trim((string) file_get_contents($file)));
+        $logFile = __DIR__ . '/../logs/bootstrap.log';
+
+        if (file_exists($logFile)) {
+            unlink($logFile);
+        }
+
+        shell_exec('php ' . escapeshellarg(__DIR__ . '/cron_common_exception.php'));
+
+        $this->assertFileExists($logFile);
+        $log = (string) file_get_contents($logFile);
+        $this->assertStringContainsString('Cron common.php failure', $log);
+
+        unlink($logFile);
     }
 }
 
