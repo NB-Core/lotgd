11f4e98b70b72c2a0e6c945267e559344bb47b9e
NB-Core <mourne@live.de>
2025-08-31 17:54:46 +0200

    feat: document censor helper functions

--- lib/censor.php
+++ lib/censor.php
@@ -1,22 +1,44 @@
 <?php
 
+declare(strict_types=1);
+
 // translator ready
 // addnews ready
 // mail ready
 
 use Lotgd\Censor;
 
-function soap($input, $debug = false, $skiphook = false)
+/**
+ * Filter a text string for banned words.
+ *
+ * @param string $input    Input string
+ * @param bool   $debug    Output debug information
+ * @param bool   $skiphook Skip module hook
+ *
+ * @return string Filtered string
+ */
+function soap(string $input, bool $debug = false, bool $skiphook = false): string
 {
     return Censor::soap($input, $debug, $skiphook);
 }
 
-function good_word_list()
+/**
+ * Retrieve exception words that bypass the filter.
+ *
+ * @return array<string> List of allowed words
+ */
+function good_word_list(): array
 {
     return Censor::goodWordList();
 }
 
-function nasty_word_list()
+/**
+ * List of banned words used by the filter.
+ *
+ * @return array<string> Compiled regexes
+ */
+function nasty_word_list(): array
 {
     return Censor::nastyWordList();
 }
+
