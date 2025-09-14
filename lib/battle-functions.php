8ad1bf6f250f71d2895c469fa1c084802f879095
NB-Core <mourne@live.de>
2025-09-14 21:54:37 +0200

    feat: add battle_spawn wrapper

--- lib/battle-functions.php
+++ lib/battle-functions.php
@@ -1,9 +1,23 @@
 <?php
 
-namespace Lotgd;
+namespace Lotgd {
+    class Fightbar extends \FightBar
+    {
+    }
 
-class Fightbar extends \FightBar
-{
+    \class_alias(Fightbar::class, 'fightbar');
 }
 
-\class_alias(Fightbar::class, 'fightbar');
+namespace {
+    use Lotgd\Battle;
+
+    /**
+     * Adds a new creature to the current battle.
+     *
+     * @param mixed $creature Creature array or numeric ID
+     */
+    function battle_spawn($creature)
+    {
+        return Battle::battleSpawn($creature);
+    }
+}
