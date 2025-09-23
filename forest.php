389877568ca33a02243aff35aaeec3fe692f337a
NB-Core <mourne@live.de>
2025-09-23 21:26:13 +0200

    refactor: adopt service layer in entry scripts

--- forest.php
+++ forest.php
@@ -11,26 +11,35 @@ use Lotgd\Page\Header;
 use Lotgd\Page\Footer;
 use Lotgd\Http;
 use Lotgd\Modules\HookHandler;
+use Lotgd\Output;
+use Lotgd\Settings;
+use Lotgd\Random;
+use Lotgd\Events;
+use Lotgd\DateTime;
+use Lotgd\Partner;
+use Lotgd\CreateString;
 
 // addnews ready
 // translator ready
 // mail ready
 require_once __DIR__ . "/common.php";
-require_once __DIR__ . "/lib/events.php";
+
+$output = Output::getInstance();
+$settings = Settings::getInstance();
 
 
 Translator::getInstance()->setSchema("forest");
 
 $fight = false;
 Header::pageHeader("The Forest");
-$dontdisplayforestmessage = handle_event("forest");
+$dontdisplayforestmessage = Events::handleEvent("forest");
 
 $op = Http::get("op");
 
 $battle = false;
 
 if ($op == "run") {
-    if (e_rand() % 3 == 0) {
+    if (Random::eRand() % 3 == 0) {
         $output->output("`c`b`&You have successfully fled your opponent!`0`b`c`n");
         $op = "";
         Http::set('op', "");
@@ -46,7 +55,6 @@ if ($op == "run") {
 }
 
 if ($op == "dragon") {
-    require_once __DIR__ . "/lib/partner.php";
     Nav::add("Enter the cave", "dragon.php");
     Nav::add("Run away like a baby", "inn.php?op=fleedragon");
     $output->output("`\$You approach the blackened entrance of a cave deep in the forest, though the trees are scorched to stumps for a hundred yards all around.");
@@ -56,13 +64,13 @@ if ($op == "dragon") {
     $output->output("You cautiously approach the entrance of the cave, and as you do, you hear, or perhaps feel a deep rumble that lasts thirty seconds or so, before silencing to a breeze of sulfur-air which wafts out of the cave.");
     $output->output("The sound starts again, and stops again in a regular rhythm.`n`n");
     $output->output("You clamber up the debris pile leading to the mouth of the cave, your feet crunching on the apparent remains of previous heroes, or perhaps hors d'oeuvres.`n`n");
-    $output->output("Every instinct in your body wants to run, and run quickly, back to the warm inn, and the even warmer %s`\$.", get_partner());
+    $output->output("Every instinct in your body wants to run, and run quickly, back to the warm inn, and the even warmer %s`\$.", Partner::getPartner());
     $output->output("What do you do?`0");
     $session['user']['seendragon'] = 1;
 }
 
 if ($op == "search") {
-    checkday();
+    DateTime::checkDay();
     if ($session['user']['turns'] <= 0) {
         $output->output("`\$`bYou are too tired to search the forest any longer today.  Perhaps tomorrow you will have more energy.`b`0");
         $op = "";
@@ -74,8 +82,8 @@ if ($op == "search") {
             'sobermsg' => "`&Faced with the prospect of death, you sober up a little.`n",
             'schema' => 'forest');
         HookHandler::hook("soberup", $args);
-        if (module_events("forest", getsetting("forestchance", 15)) != 0) {
-            if (!checknavs()) {
+        if (HookHandler::moduleEvents("forest", $settings->getSetting("forestchance", 15)) != 0) {
+            if (!Nav::checkNavs()) {
                 // If we're showing the forest, make sure to reset the special
                 // and the specialmisc
                 $session['user']['specialinc'] = "";
@@ -90,9 +98,9 @@ if ($op == "search") {
             HookHandler::hook("forestsearch_noevent", array());
             $session['user']['turns']--;
             $battle = true;
-            if (e_rand(0, 2) == 1) {
-                $plev = (e_rand(1, 5) == 1 ? 1 : 0);
-                $nlev = (e_rand(1, 3) == 1 ? 1 : 0);
+            if (Random::eRand(0, 2) == 1) {
+                $plev = (Random::eRand(1, 5) == 1 ? 1 : 0);
+                $nlev = (Random::eRand(1, 3) == 1 ? 1 : 0);
             } else {
                 $plev = 0;
                 $nlev = 0;
@@ -123,27 +131,27 @@ if ($op == "search") {
             $multi = 1;
             $targetlevel = ($session['user']['level'] + $plev - $nlev );
             $mintargetlevel = $targetlevel;
-            if (getsetting("multifightdk", 10) <= $session['user']['dragonkills']) {
-                if (e_rand(1, 100) <= getsetting("multichance", 25)) {
-                    $multi = e_rand(getsetting('multibasemin', 2), getsetting('multibasemax', 3));
+            if ($settings->getSetting("multifightdk", 10) <= $session['user']['dragonkills']) {
+                if (Random::eRand(1, 100) <= $settings->getSetting("multichance", 25)) {
+                    $multi = Random::eRand($settings->getSetting('multibasemin', 2), $settings->getSetting('multibasemax', 3));
                     if ($type == "slum") {
-                        $multi -= e_rand(getsetting("multislummin", 0), getsetting("multibasemax", 1));
-                        if (e_rand(0, 1)) {
+                        $multi -= Random::eRand($settings->getSetting("multislummin", 0), $settings->getSetting("multibasemax", 1));
+                        if (Random::eRand(0, 1)) {
                             $mintargetlevel = $targetlevel - 1;
                         } else {
                             $mintargetlevel = $targetlevel - 2;
                         }
                     } elseif ($type == "thrill") {
-                        $multi += e_rand(getsetting("multithrillmin", 1), getsetting("multithrillmax", 2));
-                        if (e_rand(0, 1)) {
+                        $multi += Random::eRand($settings->getSetting("multithrillmin", 1), $settings->getSetting("multithrillmax", 2));
+                        if (Random::eRand(0, 1)) {
                             $targetlevel++;
                             $mintargetlevel = $targetlevel - 1;
                         } else {
                             $mintargetlevel = $targetlevel - 1;
                         }
                     } elseif ($type == "suicide") {
-                        $multi += e_rand(getsetting("multisuimin", 2), getsetting("multisuimax", 4));
-                        if (e_rand(0, 1)) {
+                        $multi += Random::eRand($settings->getSetting("multisuimin", 2), $settings->getSetting("multisuimax", 4));
+                        if (Random::eRand(0, 1)) {
                             $mintargetlevel = $targetlevel - 1;
                         } else {
                             $targetlevel++;
@@ -164,29 +172,29 @@ if ($op == "search") {
             if ($mintargetlevel > $targetlevel) {
                 $mintargetlevel = $targetlevel;
             }
-            debug("Creatures: $multi Targetlevel: $targetlevel Mintargetlevel: $mintargetlevel");
+            $output->debug("Creatures: $multi Targetlevel: $targetlevel Mintargetlevel: $mintargetlevel");
             $packofmonsters = false;
             if ($multi > 1) {
-                if (getsetting('allowpackmonsters', 0)) {
-                    $packofmonsters = (e_rand(0, 5) == 0); // true or false
+                if ($settings->getSetting('allowpackmonsters', 0)) {
+                    $packofmonsters = (Random::eRand(0, 5) == 0); // true or false
                 } else {
                     $packofmonsters = false; //set for later use
                 }
                 switch ($packofmonsters) {
                     case false:
                         $multicat = "";
-                        //$multicat=(getsetting('multicategory',0)?"GROUP BY creaturecategory":"");   //grouping like that is against newer sql policies, leave it for now
-                        $sql = "SELECT * FROM " . Database::prefix("creatures") . " WHERE creaturelevel <= $targetlevel AND creaturelevel >= $mintargetlevel AND forest=1 $multicat ORDER BY rand(" . e_rand() . ") LIMIT $multi";
+                        //$multicat = ($settings->getSetting('multicategory', 0)?"GROUP BY creaturecategory":"");   //grouping like that is against newer sql policies, leave it for now
+                        $sql = "SELECT * FROM " . Database::prefix("creatures") . " WHERE creaturelevel <= $targetlevel AND creaturelevel >= $mintargetlevel AND forest=1 $multicat ORDER BY rand(" . Random::eRand() . ") LIMIT $multi";
                         break;
                     case true:
-                        $sql = "SELECT * FROM " . Database::prefix("creatures") . " WHERE creaturelevel <= $targetlevel AND creaturelevel >= $mintargetlevel AND forest=1 ORDER BY rand(" . e_rand() . ") LIMIT 1";
+                        $sql = "SELECT * FROM " . Database::prefix("creatures") . " WHERE creaturelevel <= $targetlevel AND creaturelevel >= $mintargetlevel AND forest=1 ORDER BY rand(" . Random::eRand() . ") LIMIT 1";
                         break;
                 }
             } else {
-                $sql = "SELECT * FROM " . Database::prefix("creatures") . " WHERE creaturelevel <= $targetlevel AND creaturelevel >= $mintargetlevel AND forest=1 ORDER BY rand(" . e_rand() . ") LIMIT 1";
+                $sql = "SELECT * FROM " . Database::prefix("creatures") . " WHERE creaturelevel <= $targetlevel AND creaturelevel >= $mintargetlevel AND forest=1 ORDER BY rand(" . Random::eRand() . ") LIMIT 1";
             }
             $result = Database::query($sql);
-            restore_buff_fields();
+            Buffs::restoreBuffFields();
             if (Database::numRows($result) == 0) {
                 // There is nothing in the database to challenge you, let's
                 // give you a doppleganger.
@@ -207,7 +215,7 @@ if ($op == "search") {
                     $initialbadguy = Database::fetchAssoc($result);
                     $prefixs = array("Elite","Dangerous","Lethal","Savage","Deadly","Malevolent","Malignant");
                     for ($i = 0; $i < $multi; $i++) {
-                        $initialbadguy['creaturelevel'] = e_rand($mintargetlevel, $targetlevel);
+                        $initialbadguy['creaturelevel'] = Random::eRand($mintargetlevel, $targetlevel);
                                                $badguy = Outcomes::buffBadguy($initialbadguy);
                         if ($type == "thrill") {
                             // 10% more experience
@@ -228,7 +236,7 @@ if ($op == "search") {
                             $badguy['creaturedefense'] = round($badguy['creaturedefense'] * $mul, 0);
                             $badguy['creaturehealth'] = round($badguy['creaturehealth'] * $mul, 0);
                             // And mark it as an 'elite' troop.
-                            $prefixs = translate_inline($prefixs);
+                            $prefixs = Translator::translateInline($prefixs);
                             $key = array_rand($prefixs);
                             $prefix = $prefixs[$key];
                             $badguy['creaturename'] = $prefix . " " . $badguy['creaturename'];
@@ -274,7 +282,7 @@ if ($op == "search") {
                             $badguy['creaturehealth'] = round($badguy['creaturehealth'] * $mul, 0);
                             // And mark it as an 'elite' troop.
                             $prefixs = array("Elite","Dangerous","Lethal","Savage","Deadly","Malevolent","Malignant");
-                            $prefixs = translate_inline($prefixs);
+                            $prefixs = Translator::translateInline($prefixs);
                             $key = array_rand($prefixs);
                             $prefix = $prefixs[$key];
                             $badguy['creaturename'] = $prefix . " " . $badguy['creaturename'];
@@ -295,7 +303,7 @@ if ($op == "search") {
                 )
             );
             $attackstack = HookHandler::hook("forestfight-start", $attackstack);
-            $session['user']['badguy'] = createstring($attackstack);
+            $session['user']['badguy'] = CreateString::run($attackstack);
             // If someone for any reason wanted to add a nav where the user cannot choose the number of rounds anymore
             // because they are already set in the nav itself, we need this here.
             // It will not break anything else. I hope.
