06f031159686e1269acafbaa5f39dd8f80e98f02
NB-Core <mourne@live.de>
2026-08-27 21:45:42 +0200

    fix: reject invalid mount grants

--- mounts.php
+++ mounts.php
@@ -12,6 +12,7 @@ use Lotgd\Page\Footer;
 use Lotgd\Http;
 use Lotgd\Modules\HookHandler;
 use Lotgd\Settings;
+use Lotgd\Mounts;
 // addnews ready
 // mail ready
 // translator ready
@@ -142,28 +143,14 @@ if ($op == "deactivate") {
     Http::set("op", "");
     invalidatedatacache("mountdata-$id");
 } elseif ($op == "give") {
-    if ($id === null) {
-        $op = '';
-    } else {
-        $session['user']['hashorse'] = $id;
-    }
-    // changed to make use of the cached query
-    $sql = "SELECT * FROM " . Database::prefix("mounts") . " WHERE mountid = :mountId";
-    $row = $connection->executeQuery($sql, ['mountId' => $id], ['mountId' => ParameterType::INTEGER])->fetchAssociative();
-    if ($row === false) {
+    $grantResult = Mounts::grantToCurrentUser($id);
+    if ($grantResult === Mounts::GRANT_NOT_FOUND) {
         error_log('Denied mount give action: mount record was not found');
-        $op = '';
-        Http::set('op', '');
-    } else {
-        $buff = unserialize($row['mountbuff']);
+        $output->output('`$That mount could not be found, so it was not granted.`0`n');
+    } elseif ($grantResult === Mounts::GRANT_INVALID_BUFF) {
+        error_log('Denied mount give action: stored mount buff was malformed');
+        $output->output('`$That mount has invalid buff data, so it was not granted.`0`n');
     }
-    if (!isset($buff) || !is_array($buff)) {
-        $buff = [];
-    }
-    if (($buff['schema'] ?? '') == "") {
-        $buff['schema'] = "mounts";
-    }
-    Buffs::applyBuff("mount", $buff);
     $op = "";
     Http::set("op", "");
 } elseif ($op == "save") {
