d613e955ffbc11a0d9eff98752e8ab3dea1d1ed6
Oliver Brendel <admin@shinobilegends.com>
2026-09-21 09:32:00 +0200

    small typo with `$ in ' '

--- pages/graveyard/case_enter.php
+++ pages/graveyard/case_enter.php
@@ -13,7 +13,7 @@ $output->output('`)`b`cThe Mausoleum`c`b');
 $output->output('You enter the mausoleum and find yourself in a cold, stark marble chamber.');
 $output->output('The air around you carries the chill of death itself.');
 $output->output('From the darkness, two black eyes stare into your soul.');
-$output->output('A clammy grasp seems to clutch your mind, and fill it with the words of the Overlord of Death, `\$%s`) himself.`n`n', $deathoverlord);
+$output->output('A clammy grasp seems to clutch your mind, and fill it with the words of the Overlord of Death, `$%s`) himself.`n`n', $deathoverlord);
 
 $output->output("\"`7Your mortal coil has forsaken you.  Now you turn to me.  There are those within this land that have eluded my grasp and possess a life beyond life.  To prove your worth to me and earn my favor, go out and torment their souls.  Should you gain enough of my favor, I will reward you.`)\"");
 Nav::add('G?Return to the Graveyard', 'graveyard.php');
